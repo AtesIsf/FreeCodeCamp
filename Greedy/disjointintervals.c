fe7fe54361bc0ae6efce344c5427973bea45138d
AtesIsf <isfendiyar510@gmail.com>
2022-07-02 18:57:27 +0300

    Intervals

---
+++ Greedy/disjointintervals.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+Given an array of intervals, find the biggest 
+possible number of mutually disjoint intervals.
+*/
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void sort(int *arr, int *cor, int n)
+{
+    for(int i = 0; i<n-1; i++)
+        for(int j = 0; j<n-1-i; j++)
+            if (arr[j] > arr[j+1])
+            {
+                swap(&arr[j], &arr[j + 1]);
+                swap(&cor[j], &cor[j + 1]);
+            }
+}
+
+int main()
+{
+    int n, i;
+    scanf("%d", &n);
+    // Corresponding arrays
+    int start_arr[n];
+    int end_arr[n];
+
+    for (i = 0; i<n; i++)
+        scanf("%d%d", &start_arr[i], &end_arr[i]);
+
+    sort(end_arr, start_arr, n);
+    int count = 1;
+
+    for (i = 0; i<n; i++)
+    {
+        if (start_arr[i+1] > end_arr[i])
+            count+=1;
+
+    }
+
+    printf("%d\n", count);
+    return 0;
+}
